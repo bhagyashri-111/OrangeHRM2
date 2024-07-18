0e8a41e193201d140635a9d23931eb62c67751ae
Bhagyashri <bhagyashri.jangale@gmail.com>
2024-07-18 14:10:50 +0530

    PIMPage TC Written By Tester1

--- src/test/java/Steps/PIMPageTestStep.java
+++ src/test/java/Steps/PIMPageTestStep.java
@@ -1,5 +1,47 @@
 package Steps;
 
-public class PIMPageTestStep {
+import BaseLayer.BaseClass;
+import PageLayer.PIMPage;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 
+public class PIMPageTestStep extends BaseClass{
+	
+	private static PIMPage pimPage;
+
+
+@Given("user is on PIM Page click on PIM link")
+public void user_is_on_pim_page_click_on_pim_link() {
+
+	 pimPage=new PIMPage();
+	 
+	 pimPage.clickOnPIMLink();
+	 
+	
+
+}
+
+@When("User click on add employee button")
+public void user_click_on_add_employee_button() {
+	
+	pimPage.clickOnAddEmpButton();
+   
+}
+
+@Then("create employee enter {string},{string} and {string}")
+public void create_employee_enter_and(String FirstName, String MiddleName, String LastName) {
+	
+	pimPage.createEmployee(FirstName, MiddleName, LastName);
+
+
+}
+
+@Then("click on saveButton")
+public void click_on_save_button() {
+	
+	pimPage.clickOnSaveButton();
+ 
+}
+	
 }
